57cf2a0835955be5654943135e6ff7b8af9d7adc
jorgevaz47 <jav200021@gmail.com>
2023-02-19 23:32:36 -0800

    General setup for one particle configured. Applying it to an array of particles now

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 
 using namespace std;
 
-const int NUM_PARTICLES = 10;
+const int NUM_PARTICLES = 1;
+float GRAVITY = -9.81;
+
 Particle particles[NUM_PARTICLES];
 static mt19937 generator(static_cast<long unsigned int>(chrono::high_resolution_clock::now().time_since_epoch().count()));
 uniform_int_distribution<int> height(50, 100);
@@ -28,12 +30,38 @@ void initializeParticles(){
 
 }
 
+Vector2D calculateForce(Particle particle, Vector2D acceleration){
+    return Vector2D(particle.GetMass() * acceleration.GetX(), particle.GetMass() * acceleration.GetY());
+}
+
 int main(){
 
     initializeParticles();
 
-    for(Particle p : particles){
-        cout << p << endl;
-    }
+    int y_pos = height(generator);
+    int x_vel = velocity(generator);
+    int y_vel = velocity(generator);
+    int part_mass = mass(generator);
+
+    Vector2D velocity = Vector2D(x_vel, y_vel);
+    Vector2D position = Vector2D(0, y_pos);
+    Particle test_Part = Particle(position, velocity, part_mass);
+
+    cout << "Initial Particle: " << test_Part << endl;
+
+    Vector2D gAccVec = Vector2D(0, GRAVITY);
+    Vector2D gForce = calculateForce(test_Part, gAccVec);
+
+    cout << gForce << endl;
+
+    Vector2D newVel = Vector2D(test_Part.GetVelocity().GetX() + (gForce.GetX() / test_Part.GetMass()) * 1.0, test_Part.GetVelocity().GetY() + (gForce.GetY() / test_Part.GetMass()) * 1.0);
+    Vector2D newPos = Vector2D(test_Part.GetPosition().GetX() + test_Part.GetVelocity().GetX() * 1.0, test_Part.GetPosition().GetY() + test_Part.GetVelocity().GetY() * 1.0);
+   
+    cout << "New Pos Vec: " << newPos << "\tNew Vel Vec: " << newVel << endl;
+
+    test_Part.SetPosition(newPos);
+    test_Part.SetVelocity(newVel);
+
+    cout << "Final Particle: " << test_Part << endl;
 
 }
