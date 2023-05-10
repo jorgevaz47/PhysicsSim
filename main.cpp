5378d0f60808f576b9d4b3ab0059bc3134dbd679
jorgevaz47 <jav200021@gmail.com>
2023-05-10 02:57:23 -0700

    Implemented a visualization of the particle(s) path on the window using SDL2

--- main.cpp
+++ main.cpp
@@ -10,110 +10,141 @@
 
 using namespace std;
 
-const int NUM_PARTICLES = 1;
-const int WIDTH = 800, HEIGHT = 600;
-float GRAVITY = -9.81;
+// Initializing Constants
+const int NUM_PARTICLES = 5; // Number of particles to simulate
+const int WIDTH = 800, HEIGHT = 600; // Parameters for SDL window
+const float GRAVITY = 9.81; // Acceleration due to gravity (m/s^2)
 
+// Allocating an array with size NUM_PARTICLES of Particle objects
 Particle particles[NUM_PARTICLES];
+
+// Necessary objects to make randomized parameters for particles
 static mt19937 generator(static_cast<long unsigned int>(chrono::high_resolution_clock::now().time_since_epoch().count()));
+uniform_int_distribution<int> length(50, 500);
 uniform_int_distribution<int> height(50, 100);
 uniform_int_distribution<int> velocity(0, 25);
 uniform_int_distribution<int> mass(1, 15);
 
+// This function works by creating particle objects with random parameters
+// It then places them into the array particles
 void initializeParticles()
 {
-
     for (int i = 0; i < NUM_PARTICLES; i++)
-    {
+    {   
+        int x_pos = length(generator);
         int y_pos = height(generator);
         int x_vel = velocity(generator);
         int y_vel = velocity(generator);
         int part_mass = mass(generator);
-        particles[i] = Particle(Vector2D(0, y_pos), Vector2D(x_vel, y_vel), part_mass);
+        particles[i] = Particle(Vector2D(x_pos, y_pos), Vector2D(x_vel, y_vel), part_mass);
     }
 }
 
+// Calculutes the force on a particle given an accleration due to a force
+// In this case the acceleration is due to gravity
+// Returns a Vector2D representing the force vector
+// Follows Newton's second law of motion (F=ma)
 Vector2D calculateForce(Particle particle, Vector2D acceleration)
 {
     return Vector2D(particle.GetMass() * acceleration.GetX(), particle.GetMass() * acceleration.GetY());
 }
 
+// TODO: Create function so that it encapsulates the updating of particle's position and velocity
+//       Also use another function to detect for boundaries
+void updateParticle()
+{
+}
+
 int main(int argc, char *argv[])
 {
-
-    int tFinal = 10;
-    int tInitial;
-    int tChange = 1;
-
+    // Creating the acceleration vector due to gravity
     Vector2D gAccVec = Vector2D(0, GRAVITY);
     Vector2D gForce;
 
+    // Calling method to setup all particles
     initializeParticles();
 
-    int i = 0;
-
+    // Initializing the SDL library
     SDL_Init(SDL_INIT_EVERYTHING);
 
+    // Generates a pointer to an SDL_Window with the width and height set 
     SDL_Window *window = SDL_CreateWindow("Test run", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
+    // Generates a poitner to an SDL_Renderer that is used to display graphics
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    
-
-    if(NULL == window){
+    // This checks to see if the window failed to create
+    if (NULL == window)
+    {
         cout << "Could not create window: " << SDL_GetError() << endl;
         return 1;
     }
 
-    SDL_Event windowEvent;
+    // Creating a variable to hold SDL_Event's
+    SDL_Event event;
 
-    while(true){
-        if(SDL_PollEvent(&windowEvent)){
-            if(SDL_QUIT == windowEvent.type){
-                break;
-            }
-        }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set draw color to black
+    SDL_RenderClear(renderer); // Set the entire window to black
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //Background set to black
-        SDL_RenderClear(renderer);
-
-        
-    for (Particle p : particles)
+    // This will run while the window is open
+    while (true)
     {
 
-        tInitial = 0;
+        // This checks for events from the SDL library
+        if (SDL_PollEvent(&event))
+        {   
+            // If a quit event is detect, exit while loop
+            if (SDL_QUIT == event.type)
+            {
+                break;
+            }
 
-        // Displaying initial conditions of particle
-        cout << i << " " << p << endl;
+            // Checks for a pressed key
+            if (SDL_KEYDOWN == event.type)
+            {   
+                // Checks if pressed key is the spacebar
+                if (SDLK_SPACE == event.key.keysym.sym)
+                {
+                    for (Particle &p : particles)
+                    {
 
-        // Calculate the force due to gravity of the particle (this is just for thoroughness)
-        // Only needs to be calculated once since the mass of the particle is not changing
-        gForce = calculateForce(p, gAccVec);
+                        // Calculate the force on the particle due to gravity
+                        gForce = calculateForce(p, gAccVec);
 
-        while (tInitial <= tFinal)
-        {
+                        // Calculates the new velocity and then the new position of the particle
+                        Vector2D newVel = Vector2D(p.GetVelocity().GetX() + (gForce.GetX() / p.GetMass()) * 0.1, p.GetVelocity().GetY() + (gForce.GetY() / p.GetMass()) * 0.1);
+                        Vector2D newPos = Vector2D(p.GetPosition().GetX() + p.GetVelocity().GetX() * 0.1, p.GetPosition().GetY() + p.GetVelocity().GetY() * 0.1);
 
-            // Calculating the new velocity and new position vector of the particle
-            Vector2D newVel = Vector2D(p.GetVelocity().GetX() + (gForce.GetX() / p.GetMass()) * 1.0, p.GetVelocity().GetY() + (gForce.GetY() / p.GetMass()) * 1.0);
-            Vector2D newPos = Vector2D(p.GetPosition().GetX() + p.GetVelocity().GetX() * 1.0, p.GetPosition().GetY() + p.GetVelocity().GetY() * 1.0);
+                        // FOR DEBUGGING PURPOSES UNCOMMENT THIS
+                        // cout << "New Pos: " << newPos << endl;
 
-            // Applying changes
-            p.SetVelocity(newVel);
-            p.SetPosition(newPos);
+                        // Draw a white line between the old position to the new position of the particle
+                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                        SDL_RenderDrawLine(renderer, p.GetPosition().GetX(), p.GetPosition().GetY(), newPos.GetX(), newPos.GetY());
 
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            SDL_RenderDrawPoint(renderer, p.GetPosition().GetX(), -p.GetPosition().GetY());
+                        // Draw a red point on the old position
+                        // This really can't be distinguished because of how close each point is drawn
+                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                        SDL_RenderDrawPoint(renderer, p.GetPosition().GetX(), p.GetPosition().GetY());
 
-            // Displaying velocity and position update
-            cout << i << " " << p << endl;
+                        // Update the particles velocity and position
+                        p.SetVelocity(newVel);
+                        p.SetPosition(newPos);
 
-            // Updating the time by a deltaT of 1 sec
-            tInitial += tChange;
+                        // Boundary checks
+                        // If any are out-of-bounds, flip the respective velocity
+                        if(newPos.GetX() < 50 || newPos.GetX() > 750){
+                            p.SetPosition(Vector2D(newPos.GetX(), p.GetPosition().GetY()));
+                            p.SetVelocity(Vector2D(-p.GetVelocity().GetX(), p.GetVelocity().GetY()));
+                        }
+                        if(newPos.GetY() < 50 || newPos.GetY() > 550){
+                            p.SetPosition(Vector2D(p.GetPosition().GetX(), newPos.GetY()));
+                            p.SetVelocity(Vector2D(p.GetVelocity().GetX(), -p.GetVelocity().GetY()));
+                        }
+                    }
+                }
+            }
         }
-
-        i++;
-    }
-
         SDL_RenderPresent(renderer);
     }
 
@@ -122,10 +153,4 @@ int main(int argc, char *argv[])
     SDL_Quit();
 
     return EXIT_SUCCESS;
-
-    
-
-
-    RigidBody2D test = RigidBody2D();
-
 }
